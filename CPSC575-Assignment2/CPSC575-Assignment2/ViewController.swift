4070ce1aff6cea4c19056e5f58197aef54109f53
Rei Tsunemi <94246019+rei-tsunemi@users.noreply.github.com>
2024-09-25 14:20:58 -0600

    Initial Commit

---
+++ CPSC575-Assignment2/CPSC575-Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC575-Assignment2
+//
+//  Created by Rei Tsunemi on 2024-09-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
