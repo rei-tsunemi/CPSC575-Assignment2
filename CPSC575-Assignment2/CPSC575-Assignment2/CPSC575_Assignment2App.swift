af568cc9ec1d253cc49318a627221d6bc5283191
Rei Tsunemi <94246019+rei-tsunemi@users.noreply.github.com>
2024-09-25 13:05:13 -0600

    Initial Commit

---
+++ CPSC575-Assignment2/CPSC575-Assignment2/CPSC575_Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  CPSC575_Assignment2App.swift
+//  CPSC575-Assignment2
+//
+//  Created by Rei Tsunemi on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct CPSC575_Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPSC575-Assignment2/CPSC575-Assignment2UITests/CPSC575_Assignment2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CPSC575_Assignment2UITestsLaunchTests.swift
+//  CPSC575-Assignment2UITests
+//
+//  Created by Rei Tsunemi on 2024-09-25.
+//
+
+import XCTest
+
+final class CPSC575_Assignment2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
